f8300ddd420d4727bf96314c223c13d9c67b7082
Adam Retter <adam.retter@googlemail.com>
2024-09-14 16:50:43 +0100

    Set cityEHR (http://localhost:8080/cityehr) as the default home page in Firefox

--- cityehr.pp
+++ cityehr.pp
@@ -5,6 +5,8 @@
 $cityehr_version = '1.8.0-SNAPSHOT'
 $cityehr_war_path = '/opt/tomcat/webapps/cityehr.war'
 
+$firefox_profile_id = 'ki59z67a'
+
 exec { 'download-cityehr':
   command => "curl -L https://openhealthinformatics.com/wp-content/resources/cityehr-webapp-${cityehr_version}.war -o ${cityehr_war_path}",
   path    => '/usr/bin',
@@ -15,5 +17,88 @@ exec { 'download-cityehr':
     Package['file'],
     Package['curl'],
     Service['tomcat']
+  ],
+}
+
+# Set homepage for cityEHR
+
+file { "/home/${default_user}/snap":
+  ensure => directory,
+  owner  => $default_user,
+  group  => $default_user,
+  mode   => '0700',
+}
+
+file { "/home/${default_user}/snap/firefox":
+  ensure  => directory,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0755',
+  require => File["/home/${default_user}/snap"],
+}
+
+file { "/home/${default_user}/snap/firefox/common":
+  ensure  => directory,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0755',
+  require => File["/home/${default_user}/snap/firefox"],
+}
+
+file { "/home/${default_user}/snap/firefox/common/.mozilla":
+  ensure  => directory,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0700',
+  require => File["/home/${default_user}/snap/firefox/common"],
+}
+
+file { "/home/${default_user}/snap/firefox/common/.mozilla/firefox":
+  ensure  => directory,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0700',
+  require => File["/home/${default_user}/snap/firefox/common/.mozilla"],
+}
+
+file { "/home/${default_user}/snap/firefox/common/.mozilla/firefox/profiles.ini":
+  ensure  => file,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0664',
+  content => "[Profile0]
+Name=default
+IsRelative=1
+Path=${firefox_profile_id}.default
+Default=1
+
+[General]
+StartWithLastProfile=1
+Version=2",
+  require => [
+    File["/home/${default_user}/snap/firefox/common/.mozilla/firefox"],
+    Exec['download-cityehr'],
+    Package['firefox'],
+  ],
+}
+
+file { "/home/${default_user}/snap/firefox/common/.mozilla/firefox/${firefox_profile_id}.default":
+  ensure  => directory,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0700',
+  require => File["/home/${default_user}/snap/firefox/common/.mozilla/firefox"],
+}
+
+file_line { "/home/${default_user}/snap/firefox/common/.mozilla/firefox/${firefox_profile_id}.default/prefs.js":
+  ensure  => file,
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0600',
+  content => 'user_pref("browser.startup.homepage", "http://localhost:8080/cityehr");',
+  require => [
+    File["/home/${default_user}/snap/firefox/common/.mozilla/firefox/${firefox_profile_id}.default"],
+    Exec['download-cityehr'],
+    Package['firefox'],
   ],
 }
