c1fe8cc30d2d9789f81ddb9cdb2d38a29639578c
Adam Retter <adam.retter@googlemail.com>
2025-02-14 11:25:30 -0700

    Update IntelliJ to version 2024.3.2.2

--- workstation/x-intellij.pp
+++ workstation/x-intellij.pp
@@ -2,7 +2,7 @@
 # Puppet Script for IntelliJ IDEA CE on Ubuntu 24.04
 ###
 
-$intellij_idea_version = '2023.3.3'
+$intellij_idea_version = '2024.3.2.2'
 
 file { "/opt/idea-IC-${intellij_idea_version}":
   ensure  => directory,
