2963fbefe1802f45cf5858dc4e326c3d758fa179
Adam Retter <adam.retter@googlemail.com>
2025-09-14 16:05:14 +0100

    Fix download of course materials

--- workstation/course-exercises.pp
+++ workstation/course-exercises.pp
@@ -3,7 +3,7 @@
 ###
 
 $exercises_url = 'https://static.evolvedbinary.com/xmlss/Hands-on-Introduction-to-XML.zip'
-$exercises_install_path = "/home/${default_user}/Desktop/Exercises"
+$exercises_install_path = "/home/${default_user}/Desktop/Hands-on Introduction"
 
 file { $exercises_install_path:
   ensure  => directory,
@@ -12,7 +12,7 @@ file { $exercises_install_path:
 
 exec { 'download-exercises-zip':
   command => "/usr/bin/curl -L ${exercises_url} -o /tmp/Exercises.zip",
-  creates => "${exercises_install_path}/Samples",
+  creates => "${exercises_install_path}/Exercises",
   require => [
     Package['curl'],
     File[$exercises_install_path]
@@ -21,7 +21,7 @@ exec { 'download-exercises-zip':
 
 exec { 'install-exercises':
   command => "/usr/bin/unzip -o /tmp/Exercises.zip -d /home/${default_user}/Desktop",
-  creates => "${exercises_install_path}/Samples",
+  creates => "${exercises_install_path}/Exercises",
   require => [
     Package['zip'],
     Exec['download-exercises-zip'],
