cityEHR/cityehr-workshop-vm-setup

Refactor to use a module for creating desktop shortcuts

workstation/x-desktop-panel.pp
@@ -62,10 +62,10 @@ $panel_conf = @("PANEL_EOF"/L)
 
   [quicklaunch]
   alignment=Left
-  apps\1\desktop=/home/${custom_user}/Desktop/google-chrome.desktop
+  apps\1\desktop=/home/${custom_user}/Desktop/chrome.desktop
   apps\2\desktop=/home/${custom_user}/Desktop/firefox.desktop
-  apps\3\desktop=/home/${custom_user}/Desktop/qterminal.desktop
-  apps\4\desktop=/home/${custom_user}/Desktop/pcmanfm-qt.desktop
+  apps\3\desktop=/home/${custom_user}/Desktop/terminal.desktop
+  apps\4\desktop=/home/${custom_user}/Desktop/file-manager.desktop
   apps\size=4
   type=quicklaunch
 
@@ -106,7 +106,9 @@ file { 'panel':
   require => [
     Package['desktop'],
     File['dot-config-lxqt'],
-    File['google-chrome-desktop-shortcut'],
-    File['firefox-desktop-shortcut'],
+    Xdesktop::Shortcut['Chrome'],
+    Xdesktop::Shortcut['Firefox'],
+    Xdesktop::Shortcut['Terminal'],
+    Xdesktop::Shortcut['File Manager'],
   ],
 }

workstation/x-firefox.pp
@@ -7,49 +7,20 @@ package { 'firefox':
   require => Package['desktop'],
 }
 
-$firefox_desktop_shortcut =  @("FIREFOX_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=Firefox
-  Exec=/usr/bin/firefox
-  StartupNotify=true
-  Terminal=false
-  Icon=/usr/share/icons/hicolor/128x128/apps/firefox.png
-  Type=Application
-  | FIREFOX_SHORTCUT_EOF
-
-file { 'firefox-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/firefox.desktop",
-  content => $firefox_desktop_shortcut,
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Firefox':
+  application_path => '/usr/bin/firefox',
+  application_icon => '/usr/share/icons/hicolor/128x128/apps/firefox.png',
+  startup_notify   => true,
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 12,
+  },
+  require          => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    Package['firefox'],
-  ],
-}
-
-exec { 'gvfs-trust-firefox-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/firefox.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/firefox.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['firefox-desktop-shortcut'],
-}
-
-ini_setting { 'firefox-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'firefox.desktop',
-  setting => 'pos',
-  value   => '@Point(139 12)',
-  require => [
     File['desktop-items-0'],
-    File['firefox-desktop-shortcut'],
+    Package['firefox'],
   ],
 }

workstation/x-libreoffice.pp
@@ -7,74 +7,34 @@ package { 'libreoffice':
   require => Package['desktop'],
 }
 
-file { 'libreoffice-writer-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/libreoffice-writer.desktop",
-  source  => '/usr/share/applications/libreoffice-writer.desktop',
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'LibreOffice Writer':
+  shortcut_source => '/usr/share/applications/libreoffice-writer.desktop',
+  user            => $custom_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 264,
+  },
+  require         => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
+    File['desktop-items-0'],
     Package['libreoffice'],
   ],
 }
 
-exec { 'gvfs-trust-libreoffice-writer-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/libreoffice-writer.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/libreoffice-writer.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['libreoffice-writer-desktop-shortcut'],
-}
-
-file { 'libreoffice-calc-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/libreoffice-calc.desktop",
-  source  => '/usr/share/applications/libreoffice-calc.desktop',
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'LibreOffice Calc':
+  shortcut_source => '/usr/share/applications/libreoffice-calc.desktop',
+  user            => $custom_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 390,
+  },
+  require         => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
+    File['desktop-items-0'],
     Package['libreoffice'],
-  ],
-}
-
-exec { 'gvfs-trust-libreoffice-calc-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/libreoffice-calc.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/libreoffice-calc.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['libreoffice-calc-desktop-shortcut'],
-}
-
-ini_setting { 'libreoffice-writer-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'libreoffice-writer.desktop',
-  setting => 'pos',
-  value   => '@Point(139 264)',
-  require => [
-    File['desktop-items-0'],
-    File['libreoffice-writer-desktop-shortcut'],
-  ],
-}
-
-ini_setting { 'libreoffice-calc-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'libreoffice-calc.desktop',
-  setting => 'pos',
-  value   => '@Point(139 390)',
-  require => [
-    File['desktop-items-0'],
-    File['libreoffice-calc-desktop-shortcut'],
   ],
 }

workstation/x-mirth-administrator.pp
@@ -24,50 +24,19 @@ exec { 'install-mirth-administrator':
   ],
 }
 
-$mirth_administrator_desktop_entry = @("MIRTH_ADMINISTRATOR_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=Mirth Administrator
-  Exec=${mirth_administrator_path}/launcher
-  Icon=${mirth_administrator_path}/mcadministrator/unix/launcher.ico
-  Terminal=false
-  StartupNotify=false
-  GenericName=Mirth Administrator
-  | MIRTH_ADMINISTRATOR_DESKTOP_ENTRY_EOF
-
-file { 'mirth-administrator-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/mirth-administrator.desktop",
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  content => $mirth_administrator_desktop_entry,
-  require => [
+xdesktop::shortcut { 'Mirth Administrator':
+  application_path => "${mirth_administrator_path}/launcher",
+  application_icon => "${mirth_administrator_path}/mcadministrator/unix/launcher.ico",
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 393,
+    y        => 12,
+  },
+  require          => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    File[$mirth_administrator_path]
-  ],
-}
-
-exec { 'gvfs-trust-mirth-administrator-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/mirth-administrator.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/mirth-administrator.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['mirth-administrator-shortcut'],
-}
-
-ini_setting { 'mirth-administrator-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'mirth-administrator.desktop',
-  setting => 'pos',
-  value   => '@Point(393 12)',
-  require => [
     File['desktop-items-0'],
-    File['mirth-administrator-shortcut'],
+    File[$mirth_administrator_path]
   ],
 }

workstation/x-mysql-workbench.pp
@@ -19,39 +19,18 @@ package { 'mysql-workbench-community':
   ],
 }
 
-# Add Desktop shortcut
-file { 'mysql-workbench-community-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/mysql-workbench.desktop",
-  source  => '/usr/share/applications/mysql-workbench.desktop',
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'MySQL Workbench':
+  shortcut_source => '/usr/share/applications/mysql-workbench.desktop',
+  user            => $custom_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 266,
+    y        => 138,
+  },
+  require         => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    Package['mysql-workbench-community'],
-  ],
-}
-
-exec { 'gvfs-trust-mysql-workbench-community-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/mysql-workbench.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/mysql-workbench.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['mysql-workbench-community-shortcut'],
-}
-
-ini_setting { 'mysql-workbench-community-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'mysql-workbench.desktop',
-  setting => 'pos',
-  value   => '@Point(266 138)',
-  require => [
     File['desktop-items-0'],
-    File['mysql-workbench-community-shortcut'],
+    Package['mysql-workbench-community'],
   ],
 }

workstation/x-protege.pp
@@ -26,50 +26,19 @@ exec { 'download-protege-tgz':
   ],
 }
 
-$protege_desktop_shortcut = @("PROTEGE_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=Protege
-  Exec=${protege_bin}
-  Icon=${protege_path}/app/Protege.ico
-  Terminal=false
-  StartupNotify=false
-  GenericName=Protege
-  | PROTEGE_DESKTOP_ENTRY_EOF
-
-file { 'protege-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/protege.desktop",
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  content => $protege_desktop_shortcut,
-  require => [
+xdesktop::shortcut { 'Protege':
+  application_path => $protege_bin,
+  application_icon => "${protege_path}/app/Protege.ico",
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 266,
+    y        => 264,
+  },
+  require          => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    File[$protege_path]
-  ],
-}
-
-exec { 'gvfs-trust-protege-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/protege.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/protege.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['protege-shortcut'],
-}
-
-ini_setting { 'protege-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'protege.desktop',
-  setting => 'pos',
-  value   => '@Point(266 264)',
-  require => [
     File['desktop-items-0'],
-    File['protege-shortcut'],
+    File[$protege_path],
   ],
 }

workstation/x-vscode.pp
@@ -25,38 +25,18 @@ file_line { 'vscode-no-open-folder':
   require => Package['code'],
 }
 
-file { 'vscode-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/code.desktop",
-  source  => '/usr/share/applications/code.desktop',
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Code':
+  shortcut_source => '/usr/share/applications/code.desktop',
+  user            => $custom_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 768,
+  },
+  require         => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    File_line['vscode-no-open-folder'],
-  ],
-}
-
-exec { 'gvfs-trust-vscode-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/code.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/code.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['vscode-desktop-shortcut'],
-}
-
-ini_setting { 'vscode-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'code.desktop',
-  setting => 'pos',
-  value   => '@Point(139 768)',
-  require => [
     File['desktop-items-0'],
-    File['vscode-desktop-shortcut'],
+    File_line['vscode-no-open-folder'],
   ],
 }

xmlsummerschool/xmlss-xml-intro-vm-setup

workstation/x-desktop-panel.pp
@@ -62,10 +62,10 @@ $panel_conf = @("PANEL_EOF"/L)
 
   [quicklaunch]
   alignment=Left
-  apps\1\desktop=/home/${default_user}/Desktop/google-chrome.desktop
+  apps\1\desktop=/home/${default_user}/Desktop/chrome.desktop
   apps\2\desktop=/home/${default_user}/Desktop/firefox.desktop
-  apps\3\desktop=/home/${default_user}/Desktop/qterminal.desktop
-  apps\4\desktop=/home/${default_user}/Desktop/pcmanfm-qt.desktop
+  apps\3\desktop=/home/${default_user}/Desktop/terminal.desktop
+  apps\4\desktop=/home/${default_user}/Desktop/file-manager.desktop
   apps\size=4
   type=quicklaunch
 
@@ -106,7 +106,9 @@ file { 'panel':
   require => [
     Package['desktop'],
     File['dot-config-lxqt'],
-    File['google-chrome-desktop-shortcut'],
-    File['firefox-desktop-shortcut'],
+    Xdesktop::Shortcut['Chrome'],
+    Xdesktop::Shortcut['Firefox'],
+    Xdesktop::Shortcut['Terminal'],
+    Xdesktop::Shortcut['File Manager'],
   ],
 }

workstation/x-firefox.pp
@@ -7,49 +7,20 @@ package { 'firefox':
   require => Package['desktop'],
 }
 
-$firefox_desktop_shortcut =  @("FIREFOX_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=Firefox
-  Exec=/usr/bin/firefox
-  StartupNotify=true
-  Terminal=false
-  Icon=/usr/share/icons/hicolor/128x128/apps/firefox.png
-  Type=Application
-  | FIREFOX_SHORTCUT_EOF
-
-file { 'firefox-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/firefox.desktop",
-  content => $firefox_desktop_shortcut,
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Firefox':
+  application_path => '/usr/bin/firefox',
+  application_icon => '/usr/share/icons/hicolor/128x128/apps/firefox.png',
+  startup_notify   => true,
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 12,
+  },
+  require          => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    Package['firefox'],
-  ],
-}
-
-exec { 'gvfs-trust-firefox-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/firefox.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/firefox.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['firefox-desktop-shortcut'],
-}
-
-ini_setting { 'firefox-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'firefox.desktop',
-  setting => 'pos',
-  value   => '@Point(139 12)',
-  require => [
     File['desktop-items-0'],
-    File['firefox-desktop-shortcut'],
+    Package['firefox'],
   ],
 }

workstation/x-libreoffice.pp
@@ -7,74 +7,34 @@ package { 'libreoffice':
   require => Package['desktop'],
 }
 
-file { 'libreoffice-writer-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/libreoffice-writer.desktop",
-  source  => '/usr/share/applications/libreoffice-writer.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'LibreOffice Writer':
+  shortcut_source => '/usr/share/applications/libreoffice-writer.desktop',
+  user            => $default_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 264,
+  },
+  require         => [
     Package['desktop'],
     File['default_user_desktop_folder'],
+    File['desktop-items-0'],
     Package['libreoffice'],
   ],
 }
 
-exec { 'gvfs-trust-libreoffice-writer-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/libreoffice-writer.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/libreoffice-writer.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['libreoffice-writer-desktop-shortcut'],
-}
-
-file { 'libreoffice-calc-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/libreoffice-calc.desktop",
-  source  => '/usr/share/applications/libreoffice-calc.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'LibreOffice Calc':
+  shortcut_source => '/usr/share/applications/libreoffice-calc.desktop',
+  user            => $default_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 390,
+  },
+  require         => [
     Package['desktop'],
     File['default_user_desktop_folder'],
+    File['desktop-items-0'],
     Package['libreoffice'],
-  ],
-}
-
-exec { 'gvfs-trust-libreoffice-calc-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/libreoffice-calc.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/libreoffice-calc.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['libreoffice-calc-desktop-shortcut'],
-}
-
-ini_setting { 'libreoffice-writer-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'libreoffice-writer.desktop',
-  setting => 'pos',
-  value   => '@Point(139 264)',
-  require => [
-    File['desktop-items-0'],
-    File['libreoffice-writer-desktop-shortcut'],
-  ],
-}
-
-ini_setting { 'libreoffice-calc-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'libreoffice-calc.desktop',
-  setting => 'pos',
-  value   => '@Point(139 390)',
-  require => [
-    File['desktop-items-0'],
-    File['libreoffice-calc-desktop-shortcut'],
   ],
 }

workstation/x-mirth-administrator.pp
@@ -24,50 +24,19 @@ exec { 'install-mirth-administrator':
   ],
 }
 
-$mirth_administrator_desktop_entry = @("MIRTH_ADMINISTRATOR_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=Mirth Administrator
-  Exec=${mirth_administrator_path}/launcher
-  Icon=${mirth_administrator_path}/mcadministrator/unix/launcher.ico
-  Terminal=false
-  StartupNotify=false
-  GenericName=Mirth Administrator
-  | MIRTH_ADMINISTRATOR_DESKTOP_ENTRY_EOF
-
-file { 'mirth-administrator-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/mirth-administrator.desktop",
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  content => $mirth_administrator_desktop_entry,
-  require => [
+xdesktop::shortcut { 'Mirth Administrator':
+  application_path => "${mirth_administrator_path}/launcher",
+  application_icon => "${mirth_administrator_path}/mcadministrator/unix/launcher.ico",
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 393,
+    y        => 12,
+  },
+  require          => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    File[$mirth_administrator_path]
-  ],
-}
-
-exec { 'gvfs-trust-mirth-administrator-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/mirth-administrator.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/mirth-administrator.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['mirth-administrator-shortcut'],
-}
-
-ini_setting { 'mirth-administrator-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'mirth-administrator.desktop',
-  setting => 'pos',
-  value   => '@Point(393 12)',
-  require => [
     File['desktop-items-0'],
-    File['mirth-administrator-shortcut'],
+    File[$mirth_administrator_path]
   ],
 }

workstation/x-mysql-workbench.pp
@@ -19,39 +19,18 @@ package { 'mysql-workbench-community':
   ],
 }
 
-# Add Desktop shortcut
-file { 'mysql-workbench-community-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/mysql-workbench.desktop",
-  source  => '/usr/share/applications/mysql-workbench.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'MySQL Workbench':
+  shortcut_source => '/usr/share/applications/mysql-workbench.desktop',
+  user            => $default_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 266,
+    y        => 138,
+  },
+  require         => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    Package['mysql-workbench-community'],
-  ],
-}
-
-exec { 'gvfs-trust-mysql-workbench-community-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/mysql-workbench.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/mysql-workbench.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['mysql-workbench-community-shortcut'],
-}
-
-ini_setting { 'mysql-workbench-community-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'mysql-workbench.desktop',
-  setting => 'pos',
-  value   => '@Point(266 138)',
-  require => [
     File['desktop-items-0'],
-    File['mysql-workbench-community-shortcut'],
+    Package['mysql-workbench-community'],
   ],
 }

workstation/x-protege.pp
@@ -26,50 +26,19 @@ exec { 'download-protege-tgz':
   ],
 }
 
-$protege_desktop_shortcut = @("PROTEGE_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=Protege
-  Exec=${protege_bin}
-  Icon=${protege_path}/app/Protege.ico
-  Terminal=false
-  StartupNotify=false
-  GenericName=Protege
-  | PROTEGE_DESKTOP_ENTRY_EOF
-
-file { 'protege-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/protege.desktop",
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  content => $protege_desktop_shortcut,
-  require => [
+xdesktop::shortcut { 'Protege':
+  application_path => $protege_bin,
+  application_icon => "${protege_path}/app/Protege.ico",
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 266,
+    y        => 264,
+  },
+  require          => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    File[$protege_path]
-  ],
-}
-
-exec { 'gvfs-trust-protege-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/protege.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/protege.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['protege-shortcut'],
-}
-
-ini_setting { 'protege-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'protege.desktop',
-  setting => 'pos',
-  value   => '@Point(266 264)',
-  require => [
     File['desktop-items-0'],
-    File['protege-shortcut'],
+    File[$protege_path],
   ],
 }

workstation/x-vscode.pp
@@ -25,38 +25,18 @@ file_line { 'vscode-no-open-folder':
   require => Package['code'],
 }
 
-file { 'vscode-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/code.desktop",
-  source  => '/usr/share/applications/code.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Code':
+  shortcut_source => '/usr/share/applications/code.desktop',
+  user            => $default_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 768,
+  },
+  require         => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    File_line['vscode-no-open-folder'],
-  ],
-}
-
-exec { 'gvfs-trust-vscode-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/code.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/code.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['vscode-desktop-shortcut'],
-}
-
-ini_setting { 'vscode-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'code.desktop',
-  setting => 'pos',
-  value   => '@Point(139 768)',
-  require => [
     File['desktop-items-0'],
-    File['vscode-desktop-shortcut'],
+    File_line['vscode-no-open-folder'],
   ],
 }