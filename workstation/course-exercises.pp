8a73871862a87ca2d15ac2100fc9c4277c0c6af2
Adam Retter <adam.retter@googlemail.com>
2025-09-15 12:03:24 +0100

    Fix permissions on Course Exercises folder

--- workstation/course-exercises.pp
+++ workstation/course-exercises.pp
@@ -8,6 +8,9 @@ $exercises_install_path = "/home/${default_user}/Desktop/Hands-on Introduction"
 file { $exercises_install_path:
   ensure  => directory,
   require => File['default_user_desktop_folder'],
+  owner   => $default_user,
+  group   => $default_user,
+  mode    => '0770',
 }
 
 exec { 'download-exercises-zip':
@@ -22,6 +25,7 @@ exec { 'download-exercises-zip':
 exec { 'install-exercises':
   command => "/usr/bin/unzip -o /tmp/Exercises.zip -d /home/${default_user}/Desktop",
   creates => "${exercises_install_path}/Exercises",
+  user    => $default_user,
   require => [
     Package['zip'],
     Exec['download-exercises-zip'],
